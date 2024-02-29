fefc12408f846f33915b5830e3f4ec8c9203d048
Oluwaseyi Adisa <oluwaseyiadisaa@gmail.com>
2024-02-29 12:52:28 +0100

    Change vite config

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -5,8 +5,4 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), basicSsl()],
-  build: {
-    outDir: './docs'
-  },
-  base: '/vite-boilerplate/'
 });
