832263818bee68e00938b0441d83542b92622f75
boredoreo <134864203+boredoreo@users.noreply.github.com>
2024-03-30 23:47:09 +0100

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,7 +1,9 @@
 from .start import start_handler
 from .account_creation import create_account_handler
-
+from .update_details import view_details_handler,update_details_handler
 handlers = [
     start_handler,
-    create_account_handler
+    create_account_handler, 
+    view_details_handler,
+    update_details_handler
 ]
