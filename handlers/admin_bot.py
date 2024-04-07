c5b9e696dd2f57435d5b1527a5b1a0b3463bbe4d
boredoreo <134864203+boredoreo@users.noreply.github.com>
2024-04-07 06:16:22 +0100

    Update admin_bot.py

--- handlers/admin_bot.py
+++ handlers/admin_bot.py
@@ -1,13 +1,18 @@
+import os
 from telegram.ext import (
-    CallbackQueryHandler, 
-    ConversationHandler,
-    MessageHandler,
-    filters
+    CommandHandler,
 )
-from telegram import (
-    InlineKeyboardButton,
-    InlineKeyboardMarkup
-)
+from extras.keyboards import ADMIN_START_KEYBOARD, form_keyboard
+from extras.helper_functions import sort_excel_data
+
+async def start(update, context):
+    chat_id = update.effective_chat.id
+    message = await context.bot.send_message(
+        chat_id=chat_id,
+        text="Hi, what would you want to do today?",
+        reply_markup = form_keyboard(ADMIN_START_KEYBOARD)
+    ) 
+    context.user_data["prev_message"] = message.message_id
 
 
 async def get_today_orders(update, context):
@@ -15,5 +20,12 @@ async def get_today_orders(update, context):
     await context.bot.edit_message_text(
         chat_id=chat_id,
         message_id = context.user_data["prev_message"],
-        text="Let's start setting up your account!\nWhat's your name?\n**This is what our dispatchers will yell when searching for you😗**",
+        text="Here are today's orders!",
     )
+    await sort_excel_data(send_file, update, context)
+
+def send_file(update, context, filename):
+    context.bot.send_document(update.effective_chat.id, open(filename, 'rb'))
+    os.remove(filename)
+
+admin_start_handler = CommandHandler("start", get_today_orders)
