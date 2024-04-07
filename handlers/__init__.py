ce5f6f17f5989f5c1a3e131c60785e9398d93dd6
boredoreo <134864203+boredoreo@users.noreply.github.com>
2024-04-07 06:26:28 +0100

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,11 +1,23 @@
+#user
 from .start import start_handler
 from .account_creation import create_account_handler
 from .update_details import view_details_handler,update_details_handler
 from .payment_method import payment_flow_handler
-handlers = [
+
+#admin
+from .admin_bot import admin_start_handler
+
+
+#user handlers
+user_handlers = [
     start_handler,
     create_account_handler, 
     view_details_handler,
     update_details_handler,
     payment_flow_handler
+]
+
+#admin handlers
+admin_handlers = [
+    admin_start_handler,
 ]
